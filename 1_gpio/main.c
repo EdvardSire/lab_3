b43b465b06abe641301286c172d030c7a20c19f5
Edvard Sire <mail@edvardsire.com>
2024-04-16 12:57:22 +0200

    feat: task 1 gpio

--- 1_gpio/main.c
+++ 1_gpio/main.c
@@ -1,9 +1,22 @@
 #include <stdint.h>
 
+#define OUTPUT 1
+#define __GPIO_BASE_ADDRESS__ 0x50000000
+#define __RESERVED0_SIZE__ 321
+#define __RESERVED1_SIZE__ 120
+#define __PIN_CNF_SIZE__ 32
+#define __BUTTON_1_PIN__ 15
+#define __BUTTON_2_PIN__ 16
+// #define __BUTTON_3_PIN__ 15
+// #define __BUTTON_4_PIN__ 16
+#define __LED_1_PIN__ 17
+#define __LED_2_PIN__ 18
+#define __LED_3_PIN__ 19
+#define __LED_4_PIN__ 20
 #define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
 
 typedef struct {
-	volatile uint32_t RESERVED0[321];
+	volatile uint32_t RESERVED0[__RESERVED0_SIZE__];
 	volatile uint32_t OUT;
 	volatile uint32_t OUTSET;
 	volatile uint32_t OUTCLR;
@@ -12,31 +25,63 @@ typedef struct {
 	volatile uint32_t DIRSET;
 	volatile uint32_t DIRCLR;
 	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
-	volatile uint32_t PIN_CNF[32];
+	volatile uint32_t PIN_CNF[__PIN_CNF_SIZE__];
 } NRF_GPIO_REGS;
 
-int main(){
-	// Configure LED Matrix
-	for(int i = 0; i <= 3; i++){
-		GPIO->DIRSET = (1 << i);
-		GPIO->OUTCLR = (1 << i);
-	}
 
-	// Configure buttons
-	GPIO->PIN_CNF[__BUTTON_1_PIN__] = 0;
-	GPIO->PIN_CNF[__BUTTON_2_PIN__] = 0;
+void ledMatrixOn() {
+	for(int pin_index = __LED_1_PIN__; pin_index <= (__LED_4_PIN__); pin_index++)
+		GPIO->OUTCLR = (1<<pin_index);
+};
+
+void ledMatrixOff() {
+	for(int pin_index = __LED_1_PIN__; pin_index <= (__LED_4_PIN__); pin_index++)
+		GPIO->OUTSET = (1<<pin_index);
+}
+
+
+int main(){
+	GPIO->PIN_CNF[__BUTTON_1_PIN__] = 0xC;
+	GPIO->PIN_CNF[__BUTTON_2_PIN__] = 0xC;
+	for(int pin_index = __LED_1_PIN__; pin_index <= (__LED_4_PIN__); pin_index++)
+		GPIO->PIN_CNF[pin_index] = OUTPUT;
+
+
 
 	int sleep = 0;
-	while(1){
-
+	// volatile int mask;
+	// volatile int value;
+	// volatile int flag;
+	#define BUTTON_1_MASK (1<<__BUTTON_1_PIN__)
+	#define BUTTON_2_MASK (1<<__BUTTON_2_PIN__)
+	while(1) {
 		/* Check if button 1 is pressed;
 		 * turn on LED matrix if it is. */
+		// mask  = 1<<__BUTTON_1_PIN__;
+		// value = GPIO->IN;
+		// flag = value & mask;
+		if(!(GPIO->IN & BUTTON_1_MASK))
+			ledMatrixOn();
+
 
 		/* Check if button 2 is pressed;
 		 * turn off LED matrix if it is. */
+		// mask  = 1<<__BUTTON_2_PIN__;
+		// value = GPIO->IN;
+		// flag = value & mask;
+		if(!(GPIO->IN & BUTTON_2_MASK))
+			ledMatrixOff();
 
 		sleep = 10000;
 		while(--sleep); // Delay
+
+		// Manual sleep
+		// ledMatrixOn() ;
+		// sleep = 10000*1000;
+		// while(--sleep); // Delay
+		// ledMatrixOff();
+		// sleep = 10000*1000;
+		// while(--sleep); // Delay
 	}
 	return 0;
 }
